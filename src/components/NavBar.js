be91ef80a1cf5e56aa0bcb86acab188334335efa
Mohamad Ziada <mhd.m.ziada@gmail.com>
2021-06-13 20:34:15 +0300

    all

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,17 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const renderLinks = links.map((link) =>{
+      return(
+        <a key={link} href={`#`+link}>{link}</a>
+      )
+    })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return(
+    <nav>
+      {renderLinks}
+    </nav>
+    )
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+    const technologiesList = technologies.map((tech) => {
+      return(
+        <span key={tech}>{tech}</span>
+      )}
+    );
+    return (
+      <div className="project-item">
+        <h3>{name}</h3>
+        <p>{about}</p>
+        <div className="technologies">{technologiesList}</div>
       </div>
-    </div>
-  );
+    );
 }
-
+  
 export default ProjectItem;
